e0499d6c4d5e503a2c235f2bc46712516831cd21
Cesar Gil Lao <cesargillao@hotmail.com>
2023-05-26 17:47:55 -0400

    Mostrar menú en móviles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import Logo from '../assets/images/prusked.png';
 import MenuIcon from '../assets/icons/menu.svg';
+import CloseCircleIcon from '../assets/icons/close-circle.svg';
 
 const Header = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   function openResumeBar() {
     const element = document.getElementById('resume-bar');
@@ -9,27 +13,45 @@ const Header = () => {
     element.classList.add('open');
   }
 
+  function toggleMenu() {
+    const menu = document.getElementById('link-group');
+    const isOpen = !menu.classList.contains('hidden');
+
+    if (isOpen) {
+      menu.classList.add('hidden')
+      menu.classList.remove('flex');
+    } else {
+      menu.classList.remove('hidden');
+      menu.classList.add('flex');
+    }
+
+    setIsMenuOpen(!isOpen);
+  }
+
   return (
     <div className="w-full fixed">
       <nav className="bg-white/10 h-16 w-full absolute -z-10" />
-      <nav className="h-16 px-6 lg:px-8 top-0 flex items-center justify-between" aria-label="Global">
-        <div className="flex lg:flex-1">
+      <nav className="h-16 lg:px-8 top-0 flex items-center justify-between" aria-label="Global">
+        <div className="flex lg:flex-1 pl-4">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">César Gil Lao</span>
             <img className="h-12" src={Logo} alt="Logo" />
           </a>
         </div>
-        <div className="flex lg:hidden">
-          <img src={MenuIcon} alt="Menu" />
-        </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+        <div id="link-group" className="hidden">
           <a href="#about" className="link">About</a>
-
           <a href="#experience" className="link">Experience</a>
-
           <a href="#work" className="link">Work</a>
-
           <a href="#contact" className="link">Contact</a>
+          <span
+            className="link text-blue lg:hidden"
+            onClick={() => {
+              toggleMenu();
+              openResumeBar();
+            }}
+          >
+            Resume
+          </span>
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <button
@@ -39,6 +61,14 @@ const Header = () => {
           >
             Resume
           </button>
+        </div>
+        <div className="flex pr-4 lg:hidden">
+          <img
+            className="w-7"
+            src={isMenuOpen ? CloseCircleIcon : MenuIcon}
+            alt="Menu"
+            onClick={() => toggleMenu()}
+          />
         </div>
       </nav>
     </div>
