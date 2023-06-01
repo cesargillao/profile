d0fbb29e5b4a0fec38b813a710400f7c930ee52d
Cesar Gil Lao <cesargillao@hotmail.com>
2023-06-01 08:00:57 -0400

    Agregada descripción de los trabajos

--- src/data/content.js
+++ src/data/content.js
@@ -5,7 +5,8 @@ export const data = {
     {
       "title": "Raila: Language Assistant",
       "overline": "Side Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Raila es una app potenciada con Chat GPT que ayuda a recordar e implementar palabras en distintos idiomas con el envío frecuente de recordatorios en forma de notificaciones",
+      "description_es": "Raila es una app potenciada con Chat GPT que ayuda a recordar e implementar palabras en distintos idiomas con el envío frecuente de recordatorios en forma de notificaciones",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "Flutter",
@@ -15,18 +16,19 @@ export const data = {
         {
           "title": "Raila | Google Play",
           "icon": "google-play",
-          "url": ""
+          "url": "https://play.google.com/store/apps/details?id=com.prusked.raila"
         }
       ],
     },
     {
       "title": "Mr. Parking",
-      "overline": "Side Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "overline": "Side Project (In Progress)",
+      "description": "Mr. Parking es una app cuyo objetivo principal es ubicar estacionamientos en tiempo real, dependiendo del tipo de vehículo del conductor. Y como modelo de negocio, se implementa una tienda orientada a todo tipo de vehículo",
+      "description_es": "Mr. Parking es una app cuyo objetivo principal es ubicar estacionamientos en tiempo real, dependiendo del tipo de vehículo del conductor. Y como modelo de negocio, se implementa una tienda orientada a todo tipo de vehículo",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "Flutter",
-        "NestJS",
+        "Nest.js",
         "Firebase",
         "Socket.io"
       ],
@@ -42,7 +44,8 @@ export const data = {
     {
       "title": "CreditBook",
       "overline": "Side Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "CreditBook es una app para administrar los prestamos personales, cuya paleta de colores fue inspirada por Binance. Fue la primera app que creé luego de reallizar el curso de React Native",
+      "description_es": "CreditBook es una app para administrar los prestamos personales, cuya paleta de colores fue inspirada por Binance. Fue la primera app que creé luego de reallizar el curso de React Native",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "React Native",
@@ -60,7 +63,8 @@ export const data = {
     {
       "title": "SIAT: Sistema Integrado de Administración Tributaria (Taxes)",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Este sistema es el más relevante en mi lista de proyectos ya que se creó desde cero toda la lógica de negocio, los cálculos y sus extensos formularios en una tecnología de la cual no tenía conocimiento (Angular) pero facilmente pude adaptarme. Lo más satisfactorio es saber que fue usado por miles de usuarios",
+      "description_es": "Este sistema es el más relevante en mi lista de proyectos ya que se creó desde cero toda la lógica de negocio, los cálculos y sus extensos formularios en una tecnología de la cual no tenía conocimiento (Angular) pero facilmente pude adaptarme. Lo más satisfactorio es saber que fue usado por miles de usuarios",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "Angular",
@@ -72,7 +76,8 @@ export const data = {
     {
       "title": "Prevenauto",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Aplicación para el registro de combustible y kilometraje de la flota de vehículos. Me encargué de realizar la mayoría de las pantallas para los diferentes entornos en base a un diseño proporcionado",
+      "description_es": "Aplicación para el registro de combustible y kilometraje de la flota de vehículos. Me encargué de realizar la mayoría de las pantallas para los diferentes entornos en base a un diseño proporcionado",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "React Native",
@@ -90,13 +95,14 @@ export const data = {
     {
       "title": "Below",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Below es un sistema para administar servicio técnico profesional de mantenimiento preventivo y correctivo de aire acondicionado. El cual posee una app móvil y web. Desarrollé todas las pantallas de la web y gran parte de sus funcionalidades. Participé en algunas pantallas y detalles en la app móvil incluyendo la versión para iOS y también en algunos ajustes en backend.",
+      "description_es": "Below es un sistema para administar servicio técnico profesional de mantenimiento preventivo y correctivo de aire acondicionado. El cual posee una app móvil y web. Desarrollé todas las pantallas de la web y gran parte de sus funcionalidades. Participé en algunas pantallas y detalles en la app móvil incluyendo la versión para iOS y también en algunos ajustes en backend.",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "React Native",
-        "ReactJS",
+        "React.js",
         "TypeScript",
-        "NestJS",
+        "Nest.js",
         "Firebase",
         "Socket.io",
         "Redux",
@@ -113,11 +119,12 @@ export const data = {
     {
       "title": "Tecno",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Tecno permite organizar los pedidos de los clientes por anticipado. Posee una app web que se encarga de la logística, los usuarios, productos, procesar los pedidos, cierres de caja, etc. Participé en todo el front end de ambas apps",
+      "description_es": "Tecno permite organizar los pedidos de los clientes por anticipado. Posee una app web que se encarga de la logística, los usuarios, productos, procesar los pedidos, cierres de caja, etc. Participé en todo el front end de ambas apps",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "React Native",
-        "ReactJS",
+        "React.js",
         "Laravel",
         "Firebase",
         "Socket.io",
@@ -135,12 +142,13 @@ export const data = {
     {
       "title": "SerVIP",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "SerVIP es una app en la que puedes buscar directorios comerciales por categorías, estados y ciudades. Desarrollé todas las pantallas en base a un diseño proporcionado y al cabo de un tiempo me solicitaron implementar un rediseño completo.",
+      "description_es": "SerVIP es una app en la que puedes buscar directorios comerciales por categorías, estados y ciudades. Desarrollé todas las pantallas en base a un diseño proporcionado y al cabo de un tiempo me solicitaron implementar un rediseño completo.",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "React Native",
-        "ReactJS",
-        "NestJS",
+        "React.js",
+        "Nest.js",
         "Firebase",
         "Socket.io",
         "Redux"
@@ -156,11 +164,11 @@ export const data = {
     {
       "title": "Taxealo",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Taxealo es una app como Uber. Partió desde un proyecto base y surgieron varias apps con diferentes nombres y características distintas. Implementé el rediseño de Taxealo y Liberty en sus versiones de Pasajero y Conductor. También participé en implementaciones y ajustes de la parte web administrativa y el backend",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "React Native",
-        "ReactJS",
+        "React.js",
         "Vue",
         "Laravel",
         "Firebase",
@@ -182,17 +190,22 @@ export const data = {
           "title": "Yesgo | Google Play",
           "icon": "google-play",
           "url": "https://play.google.com/store/apps/details?id=com.yesgo"
+        },
+        {
+          "title": "Liberty | Google Play",
+          "icon": "google-play",
+          "url": "https://play.google.com/store/apps/details?id=com.limonbyte.liberty"
         }
       ],
     },
     {
       "title": "Dareco",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Es una app que complementa una tienda web creada con Wordpress y WooCommerce. Implementé funcionalidades que permitieron desde la app crear clientes y autenticarlos, realizar los pedidos, gestionar costos de envío en base a la ubicación del cliente y manejo de productos con variantes",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "React Native",
-        "ReactJS",
+        "React.js",
         "Vue",
         "Laravel",
         "Firebase",
@@ -204,17 +217,22 @@ export const data = {
           "title": "Dareco | Google Play",
           "icon": "google-play",
           "url": "https://play.google.com/store/apps/details?id=com.limonbyte.dareco"
+        },
+        {
+          "title": "Dareco | Web",
+          "icon": "web",
+          "url": "https://darecoweb.com"
         }
       ],
     },
     {
       "title": "Sysge24",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Es un ERP que consta de 3 partes: Web administrativa, app móvil y tienda pública, siendo esta última desarrollada desde cero por mí, el cual sirve para realizar pedidos siendo posible enviar estos mediante WhatsApp. También me tocó implementar mejoras en la web administrativa de front end y back end, además de migrar funcionalidades existentes en la app móvil hacia la web. En la app móvil me tocó realizar un rediseño parcial",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "React Native",
-        "ReactJS",
+        "React.js",
         "Laravel",
         "Firebase",
         "Socket.io",
@@ -225,7 +243,13 @@ export const data = {
         {
           "title": "Sysge24 | Web",
           "icon": "web",
-          "url": "https://sysge24.com/"
+          "url": "https://sysge24.com"
+        },
+        {
+          // TODO: Poner la url real
+          "title": "Sysge24 | App Store",
+          "icon": "app-store",
+          "url": ""
         },
         {
           "title": "Sysge24 | Google Play",
@@ -237,10 +261,10 @@ export const data = {
     {
       "title": "Automatizac",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Automatizac es un ERP para la administración de contratos de instalación, servicio y mantenimiento a portones elécticos. Fue creado desde cero y constituido por una app web y una app móvil la cual le permite al técnico gestionar su calendario de servicios y reportar trabajos realizados",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
-        "AngularJS",
+        "Angular.js",
         "PHP",
         "SQL",
         "Cordova"
@@ -256,10 +280,11 @@ export const data = {
     {
       "title": "Cucaramatcha Restaurant POS",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Es un POS el cual se conecta a una app móvil multientornos en tiempo real para enviar los pedidos a la cocina, notificar el estatus de pedidos, reserva de mesas. Fue creado todo desde cero enteramente por mí",
+      "description_es": "Es un POS el cual se conecta a una app móvil multientornos en tiempo real para enviar los pedidos a la cocina, notificar el estatus de pedidos, reserva de mesas. Fue creado todo desde cero enteramente por mí",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
-        "AngularJS",
+        "Angular.js",
         "PHP",
         "SQL",
         "Socket.io"
@@ -269,10 +294,11 @@ export const data = {
     {
       "title": "Huyces",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "Es un ERP el cual se conecta a una app móvil que permite a los vendedores realizar pedidos de forma offline y visualizar clientes en el mapa por itinerario diario. Fue creado todo desde cero enteramente por mí",
+      "description_es": "Es un ERP el cual se conecta a una app móvil que permite a los vendedores realizar pedidos de forma offline y visualizar clientes en el mapa por itinerario diario. Fue creado todo desde cero enteramente por mí",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
-        "AngularJS",
+        "Angular.js",
         "PHP",
         "SQL",
       ],
@@ -281,7 +307,7 @@ export const data = {
     {
       "title": "PIC Microcontrollers Multiporpuse Software",
       "overline": "Featured Project",
-      "description": "Consequat quis adipisicing qui fugiat minim officia laboris. Amet nisi nostrud pariatur do deserunt consectetur eiusmod occaecat.",
+      "description": "En mi perfil de Github podrás encontrar múltiples proyectos relacionados con microcontroladores PIC, los cuales han sido desarrollados enteramente por mí y algunos de ellos los he usado en proyectos comerciales. Otros fueron realizados mediante ingeniería inversa con fines educativos para replicar el funcionamiento de equipo que yo mismo ensamblaba en una empresa para la cual trabajé en el año 2016",
       "image": "http://placeimg.com/640/480/food",
       "technologies": [
         "C",
@@ -291,9 +317,9 @@ export const data = {
       ],
       "links": [
         {
-          "title": "Repositorios",
+          "title": "Repositorios | Github",
           "icon": "web",
-          "url": ""
+          "url": "https://github.com/cesargillao"
         }
       ],
     }
