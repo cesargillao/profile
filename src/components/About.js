f475de21ce424f36cb7cd9ef65accb3c58b7b858
César Gil Lao <cesargillao@hotmail.com>
2023-06-02 17:14:08 -0400

    🔧 Espaciado de contenido en versión móvil y tablet

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,14 @@ const About = () => (
   <section id="about">
     <h2 className="title">About Me</h2>
     <div className="content">
-      <p>I am a software developer with 7 years of experience. I am passionate about creating visually striking, intuitive, scalable, and high-quality software with added value. I have extensive experience developing full web and mobile applications with technologies such as React.js, React Native, Flutter and Node.js. Also, I am always on personal side projects that allow me to learn about new tools and techniques that can improve the development process and ensure the quality of the final product.</p>
+      <p className="px-2 lg:px-0">I am a software developer with 7 years of experience. I am passionate about creating visually striking, intuitive, scalable, and high-quality software with added value. I have extensive experience developing full web and mobile applications with technologies such as React.js, React Native, Flutter and Node.js. Also, I am always on personal side projects that allow me to learn about new tools and techniques that can improve the development process and ensure the quality of the final product.</p>
       <div className="photo">
         <img src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/862.jpg" alt="César Gil Lao" />
       </div>
     </div>
-    <p className="text-gray-300 pb-2">Here are a few technologies I've been working with recently:</p>
-    <div className="content">
-      <ul className="about-skills flex-[3]">
+    <p className="text-gray-300 pt-4 pb-2 px-2 lg:px-0">Here are a few technologies I've been working with recently:</p>
+    <div className="content px-2 lg:px-0">
+      <ul className="about-skills">
         <li>React.js</li>
         <li>React Native</li>
         <li>Flutter</li>
