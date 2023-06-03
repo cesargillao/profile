bae8c97515f48a577e1586787ed5c656499ac670
Cesar Gil Lao <cesargillao@hotmail.com>
2023-06-03 12:21:26 -0400

    Información de About centralizada en content

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,18 @@
+import { data } from '../data/content';
+
 const About = () => (
   <section id="about">
     <h2 className="title">About Me</h2>
     <div className="content">
-      <p className="px-2 lg:px-0">I am a software developer with 7 years of experience. I am passionate about creating visually striking, intuitive, scalable, and high-quality software with added value. I have extensive experience developing full web and mobile applications with technologies such as React.js, React Native, Flutter and Node.js. Also, I am always on personal side projects that allow me to learn about new tools and techniques that can improve the development process and ensure the quality of the final product.</p>
+      <p className="px-2 lg:px-0">{ data.about }</p>
       <div className="photo">
-        <img src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/862.jpg" alt="César Gil Lao" />
+        <img src={data.profile_photo} alt="César Gil Lao" />
       </div>
     </div>
     <p className="text-gray-300 pt-4 pb-2 px-2 lg:px-0">Here are a few technologies I've been working with recently:</p>
     <div className="content px-2 lg:px-0">
       <ul className="about-skills">
-        <li>React.js</li>
-        <li>React Native</li>
-        <li>Flutter</li>
-        <li>TypeScript</li>
-        <li>Tailwind CSS</li>
-        <li>SASS</li>
-        <li>Nest.js</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>PHP</li>
-        <li>Laravel</li>
-        <li>SQL</li>
-        <li>Git</li>
-        <li>Redux</li>
-        <li>Socket.io</li>
+        {data.skills.map(skill => <li>{ skill }</li>)}
       </ul>
       <div className="flex-[2]" />
     </div>
--- src/data/content.js
+++ src/data/content.js
@@ -1,6 +1,25 @@
 
 
 export const data = {
+  "profile_photo": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/862.jpg",
+  "about": "I am a software developer with 7 years of experience. I am passionate about creating visually striking, intuitive, scalable, and high-quality software with added value. I have extensive experience developing full web and mobile applications with technologies such as React.js, React Native, Flutter and Node.js. Also, I am always on personal side projects that allow me to learn about new tools and techniques that can improve the development process and ensure the quality of the final product.",
+  "skills": [
+    "React.js",
+    "React Native",
+    "Flutter",
+    "TypeScript",
+    "Tailwind CSS",
+    "SASS",
+    "Nest.js",
+    "Node.js",
+    "Express.js",
+    "PHP",
+    "Laravel",
+    "SQL",
+    "Git",
+    "Redux",
+    "Socket.io"
+  ],
   "work": [
     {
       "title": "Raila: Language Assistant",
