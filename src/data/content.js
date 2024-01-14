11e9f80ba6802adfb7f12c9da42ea861213e8b65
César Gil Lao <cesargillao@hotmail.com>
2024-01-13 21:51:18 -0400

    🎉 8 años de experiencia

--- src/data/content.js
+++ src/data/content.js
@@ -19,7 +19,7 @@ import CreditBookAPK from '../assets/apk/CreditBook.apk';
 
 export const data = {
   "profile_photo": ProfilePhoto,
-  "about": "I am a software developer with 7 years of experience. I am passionate about creating visually striking, intuitive, scalable, and high-quality software with added value. I have extensive experience developing full web and mobile applications with technologies such as React.js, React Native, Flutter and Node.js. Also, I am always on personal side projects that allow me to learn about new tools and techniques that can improve the development process and ensure the quality of the final product.",
+  "about": "I am a software developer with 8 years of experience. I am passionate about creating visually striking, intuitive, scalable, and high-quality software with added value. I have extensive experience developing full web and mobile applications with technologies such as React.js, React Native, Flutter and Node.js. Also, I am always on personal side projects that allow me to learn about new tools and techniques that can improve the development process and ensure the quality of the final product.",
   "skills": [
     "React.js",
     "React Native",
